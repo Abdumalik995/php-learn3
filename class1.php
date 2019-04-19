2dc3033b2cb5dd5bf12b34d8cb952394b854bd2e
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-19 11:03:46 +0300

    vorislik asosida funksiyalar

--- class1.php
+++ class1.php
@@ -8,6 +8,11 @@
 			$vaqt = $masofa / $this->tezlik;
 			return $vaqt;
 		}
+		public function vaqt1($masofa, $os, $ov) {
+			$vaqt1 = $masofa/$this->tezlik;
+			$vaqt2 = $os * $ov/3600;
+			return ($vaqt1+$vaqt2)*60;
+		}
 	}
 
 
@@ -42,18 +47,20 @@
 
 	$matiz = new Mashina;
 
-	$matiz->tezlik = '50';
+	$matiz->tezlik = '60';
 	$matiz->benzin_sarfi = '7';
+	$matiz->svetofor_vaqti = '30';
 	//echo $matiz->hisoblash(10000);
-	echo '<br>',$matiz->benzin_hisoblash(150);
-	echo '<br>',$matiz->vaqt(100, 10);
+	//echo '<br>',$matiz->benzin_hisoblash(150);
+	echo '<br>',$matiz->vaqt(20, 5);
 
 
 	$velo = new Velosiped;
 	$velo->tezlik = '15';
-	echo '<br>',$velo->hisoblash(10000);
+	//echo '<br>',$velo->hisoblash(10000);
 
 	$isuzu = new Avtobus;
+	$isuzu->tezlik = '50';
 	$isuzu->yoqilgi_sarfi = '15';
-	echo '<br>',$isuzu->yoqilgi_hisoblash(300);
+	//echo '<br>',$isuzu->yoqilgi_hisoblash(300);
  ?>
