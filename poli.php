fcb305b4382881ea01db12e16ed7f773e9c3c23a
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-22 10:10:11 +0300

    alohida fal orqali chaqirish

--- poli.php
+++ poli.php
@@ -1,8 +1,40 @@
 <?php 
 	class Maqola {
-		public $name;
+		public $id;
+		public $name = "Mavzu1";
 		public $anonss;
 		public $texts;
 		public $img;
+		public $datee;
+		public $about;
 	}
+
+
+	class Yangiliklar extends Maqola {
+		function Chop_etish() {
+			echo "<br>Mavzusi".$this->name;
+			echo "<br>".$this->anoss;
+
+		}
+	}
+
+	class About extends Maqola {
+		function Chop_etish() {
+			echo "<br>Mavzusi".$this->name;
+			echo "<br>".$this->anoss;
+
+		}
+	}
+
+	class Galereya extends Maqola {
+		function Chop_etish() {
+			echo "<br>Mavzusi".$this->name;
+			echo "<br>".$this->anoss;
+
+		}
+	}
+
+
+
+
 ?>
---
+++ req.php
@@ -0,0 +1,14 @@
+<?php 
+	require_once("poli.php");
+
+	$yangi = new Yangiliklar;
+	echo "<pre>";
+	print_r($yangi);
+	$about = new About;
+	echo "<pre>";
+	print_r($yangi);
+	$galereya = new Galereya;
+	echo "<pre>";
+	print_r($yangi);
+
+ ?>
